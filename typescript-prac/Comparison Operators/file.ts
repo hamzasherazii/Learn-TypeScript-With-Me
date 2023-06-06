efa7375ddca6d8290c4fee9347928bc2860da1ad
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-06-06 21:18:38 +0500

    done with PIAIC playlist

--- typescript-prac/Comparison Operators/file.ts
+++ typescript-prac/Comparison Operators/file.ts
@@ -4,7 +4,7 @@ console.log(m >=5 && m < 10); // && = and
 console.log(m > 5 && m < 10);
 console.log(m >=5 || m <10); // || = or
 console.log(m > 5 || m < 10);
-console.log(!(m < 10)); // the ! behing inverts the result
+console.log(!(m < 10)); // the ! behind inverts the result
 // like it is true that m is less than 10, but ! operator will invert the result and make it false
 
 console.log(!(m > 10));
