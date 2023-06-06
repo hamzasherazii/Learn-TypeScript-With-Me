d756b03f76a5dfddf01b7628d9101ac8222bd095
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-06-06 17:08:21 +0500

    learned typeof

---
+++ typescript-prac/Variables/typeof.js
@@ -0,0 +1,6 @@
+var variable1 = 3;
+var variable2 = "this is a string";
+var variable3 = true;
+console.log(typeof variable1);
+console.log(typeof variable2);
+console.log(typeof variable3);
---
+++ typescript-prac/Variables/typeof.ts
@@ -0,0 +1,7 @@
+let variable1 = 3;
+let variable2 = "this is a string";
+let variable3 = true;
+
+console.log(typeof variable1);
+console.log(typeof variable2);
+console.log(typeof variable3);
