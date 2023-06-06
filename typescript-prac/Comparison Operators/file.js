3dfba81963065b42030bca7c24d8a9f248ba6bf5
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-06-06 17:34:28 +0500

    learned comparison operators

--- typescript-prac/Comparison Operators/file.js
+++ typescript-prac/Comparison Operators/file.js
@@ -1,10 +1,8 @@
-let m = 5;
-
-console.log(m >=5 && m < 10); // && = and
+var m = 5;
+console.log(m >= 5 && m < 10); // && = and
 console.log(m > 5 && m < 10);
-console.log(m >=5 || m <10); // || = or
+console.log(m >= 5 || m < 10); // || = or
 console.log(m > 5 || m < 10);
 console.log(!(m < 10)); // the ! behing inverts the result
 // like it is true that m is less than 10, but ! operator will invert the result and make it false
-
 console.log(!(m > 10));
