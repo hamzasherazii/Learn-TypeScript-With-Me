9756caaf0e487cfd70e16c738712963971bb0420
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-10-04 09:31:49 +0500

    parameters and arguments

---
+++ typescript-prac/Functions/params.ts
@@ -0,0 +1,7 @@
+let sum = (val1:number, val2:number) => { // idher val1 or val2 parameters hain
+    return val1 + val2;
+}
+
+let res = sum(2, 3); // idher 2 or 3 arguments hain
+
+console.log(res);
